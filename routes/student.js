127ecb875f39cc482f883c3f52bbf250ec5368b9
kumi1688 <43873915+kumi1688@users.noreply.github.com>
2022-02-22 12:54:09 +0900

    fix teacher #3

--- routes/student.js
+++ routes/student.js
@@ -11,13 +11,19 @@ router.get("/", (req, res) => {
 });
 
 router.get("/search", (req, res) => {
-  console.log(req.query);
+  // 1. sample.json에서 학생들의 데이터만 고르기
   let students = sample.students;
+
+  // 2. 클라이언트에 보내온 Query에서 Key값만 골라내기
   const keys = Object.keys(req.query);
+
+  // 3. 클라이언트가 보내온 Key의 값과 sample.json에 있는 학생 데이터 중에서 조회하고자 하는 것이 있나 확인
   for (key of keys) {
-    let filtered = students.filter((student) => student[key] == req.query[key]);
-    if (filtered.length != 0) students = filtered;
+    let filtered = students.filter((student) => student[key] == req.query[key]); // 있으면 필터링하고
+    if (filtered.length != 0) students = filtered; // 없다면 그냥 빈 배열 전달
   }
+
+  // 4. 필터링 된 데이터 전달하기
   res.send(students);
 });
 
--- routes/teacher.js
+++ routes/teacher.js
@@ -36,13 +36,21 @@ router.post("/", (req, res) => {
   // 3. 새로운 선생님의 id는 마지막 선생님 id + 1
   newTeacher["id"] = teachers.length + 1;
 
+  // 4. 새로운 선생님을 기존의 선생님 데이터에 추가
   teachers.push(newTeacher);
 
+  // 5. sample 객체에 새로운 선생님들 데이터로 덮어쓰기
   sample.teachers = teachers;
+
+  // 6. sample 객체에 쓰일 데이터 경로 명시
   filePath = path.join(__dirname, "../public/sample.json");
+
+  // 7. 기존의 sample.json을 업데이트된 sample 객체로 덮어쓰기
   fs.writeFile(filePath, JSON.stringify(sample), {}, (err) => {
     if (err) console.log(err);
   });
+
+  // 8. 업데이트 된 선생님들 데이터를 클라이언트에게 보내기
   res.send(teachers);
 });
 
