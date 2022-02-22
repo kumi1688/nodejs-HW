0689cc3a27614398eff205f235037ba2b0ab9c45
kumi1688 <43873915+kumi1688@users.noreply.github.com>
2022-02-22 12:04:56 +0900

    add public assets

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const express = require("express");
 // express 모듈이 실행되면 하나의 객체를 반환하는데, 반환된 객체를 보통 app이라고 부름
 const app = express();
 
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Express 모듈의 미들웨어 설정하기
+app.use(express.static("public")); // public 폴더 안에 있는 정적 파일들을 URL만으로 전달할 수 있는 미들웨어 적용
+app.use(express.json()); // 클라이언트의 request가 json 타입으로 오는 경우 parsing 해주는 미들웨어 적용
+app.use(express.urlencoded({ extended: true })); // 클라이언트의 request가 URL(Query)로 오는 경우 parsing 해주는 미들웨어 적용
 
+// http://localhost:3000/ 으로 접속하는 요청에 대한 응답 설정
 app.get("/", (req, res) => {
   res.send("ok");
 });
